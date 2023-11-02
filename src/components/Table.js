9ab0458d0f29d9abb59bcb63050648d5892ee4e9
Neal Uehara <ueharaneal@gmail.com>
2023-11-02 09:05:55 -0700

    Data table complete without sorting

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,40 +1,30 @@
+function Table({ data, config, keyFn }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+  const renderedRows = data.map((rowData) => {
+    const renderedCells = config.map((column) => {
+      return (
+        <td className="p-2" key={column.label}>
+          {column.render(rowData)}
+        </td>
+      );
+    });
+    return (
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderedCells}
+      </tr>
+    );
+  });
 
-function Table( {data} ){
-    const renderedRows = data.map((fruit)=>{
-        return(
-            <tr className ="border-b"key={fruit.name}>
-                <td className="p-3">{fruit.name}</td>
-                <td className="p-3">
-                    <div className={`p-3 m-2 ${fruit.color}`}>
-                        
-                    </div>                  
-                </td>
-                <td className="p-3">{fruit.score}</td>
-            </tr>
-           
-        )
-    })
-    
-    return(
-        <table className ="table-auto border-spacing-2">
-            <thead>
-                <tr className="border-b-2">
-                    <th>
-                        Fruits
-                    </th>
-                    <th>
-                        Color
-                    </th>
-                    <th>
-                        Score
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                {renderedRows}
-            </tbody>
-        </table>
-    )
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 
-export default Table
+export default Table;
