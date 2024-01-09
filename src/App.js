701a5b29144a2266dcf58808830873c12e0067ad
Neal Uehara <ueharaneal@gmail.com>
2024-01-09 15:52:35 -0800

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import Route from "./components/Route"
-import Sidebar from './components/Sidebar'
-import AccordionPage from './pages/AccordionPage'
-import DropdownPage from "./pages/DropdownPage"
-import ButtonPage from "./pages/ButtonPage"
-import ModalPage from './pages/ModalPage'
-import TablePage from "./pages/TablePage"
-function App (){
+
+function App(){
   return(
-    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4 ">
-      <Sidebar/>
-      <div className="col-span-5">
-        <Route path="/accordion">
-          <AccordionPage/>
-        </Route>
-        <Route path="/">
-          <DropdownPage/>
-        </Route>
-        <Route path="/buttons">
-          <ButtonPage/>
-        </Route>
-        <Route path="/modal">
-          <ModalPage/>
-        </Route>
-        <Route path="/table">
-          <TablePage/>
-        </Route>
-      </div>
-    </div>
+    <div></div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import "./index.css"
+import './index.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom';
 import App from './App';
-import {NavigationProvider} from './context/navigation'
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 root.render(
-   <NavigationProvider>
-        <App/>
-   </NavigationProvider>
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
